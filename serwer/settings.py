64c8897ce07a6a2545f3a107dac2d1bd6cc8b8ee
Stachuh <stachu.games.bbc@gmail.com>
2026-04-03 11:03:25 +0200

    test

--- serwer/settings.py
+++ serwer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ga0fvnz0r9f1$-!37f1*r)pna@+c%-t*^!+@akmhu#=q^w(p$1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
